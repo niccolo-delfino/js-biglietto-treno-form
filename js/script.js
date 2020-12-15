61f85677144582fb2ba03ef4be412a7bbb7ed0b5
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-15 17:46:22 +0100

    click event WIP

--- js/script.js
+++ js/script.js
@@ -5,10 +5,14 @@ function() {
   var nomeCognome = document.getElementById('nome').value;
   var kilometri = document.getElementById('km').value;
   var eta = document.getElementById('eta').value;
+  var prezzo = kilometri * 0.21
 
-  console.log(nomeCognome);
-  console.log(kilometri);
-  console.log(eta);
+  // console.log(nomeCognome);
+  // console.log(kilometri);
+  // console.log(eta);
 
+  document.getElementById('nome-pass').innerHTML = nomeCognome;
+  document.getElementById('carrozza').innerHTML = Math.floor(Math.random()*9) + 1;
+  document.getElementById('codice').innerHTML = Math.floor(Math.random() * (100000 - 90000 + 1)) + 90000;
 }
 );
