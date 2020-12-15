843554fcd739a0e41d6c143161cd0450a2e91018
Niccolo Delfino <delfino.nicco@libero.it>
2020-12-15 17:08:29 +0100

    genera ok in .log

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var element = document.getElementById("genera");
+element.addEventListener('click',
+function() {
+
+  var nomeCognome = document.getElementById('nome').value;
+  var kilometri = document.getElementById('km').value;
+  var eta = document.getElementById('eta').value;
+
+  console.log(nomeCognome);
+  console.log(kilometri);
+  console.log(eta);
+
+}
+);
